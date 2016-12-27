2dabc2ebb4f0b95aa7e75a8721faf26ea0d0fffa
Rocco <rocco@cloudplugs.com>
2016-12-27 16:25:07 +0100

    uglify JS and usiny BrowserSync as Live-reload server

--- client/gulpfile.js
+++ client/gulpfile.js
@@ -1,58 +1,73 @@
 var gulp = require('gulp');
-var browserify = require('gulp-browserify');
+//var browserify = require('gulp-browserify');
 var concat = require('gulp-concat');
-var refresh = require('gulp-livereload');
-var lr = require('tiny-lr');
-var server = lr();
+var browserSync = require('browser-sync').create();
 var minifyCSS = require('gulp-minify-css');
-var embedlr = require('gulp-embedlr');
-
-var serverPort = 2000;
+var sourcemaps = require('gulp-sourcemaps');
+var uglify = require('gulp-uglify');
 
 gulp.task('scripts', function() {
-    gulp.src(['app/src/**/*.js'])
-        .pipe(browserify())
+    gulp.src(['app/src/lib/**/*.js', 'app/src/**/*.js']) // concat first the lib dir
+        .pipe(sourcemaps.init())
         .pipe(concat('bundle.js'))
+        .pipe(uglify())
+        .pipe(sourcemaps.write())
         .pipe(gulp.dest('dist/build'))
-        .pipe(refresh(server))
 })
 
 gulp.task('styles', function() {
     gulp.src(['app/css/style.css'])
         .pipe(minifyCSS())
         .pipe(gulp.dest('dist/build'))
-        .pipe(refresh(server))
-})
-
-gulp.task('lr-server', function() {
-    server.listen(serverPort, function(err) {
-        if(err) return console.log(err);
-    });
 })
 
 gulp.task('html', function() {
     gulp.src("app/*.html")
-        .pipe(embedlr())
         .pipe(gulp.dest('dist/'))
-        .pipe(refresh(server));
 })
 
+gulp.task('assets', function(){
+    gulp.src("app/assets/**/*")
+        .pipe(gulp.dest('dist/assets'))
+});
+
+
+gulp.task('lr-server', function() { // live reload server
+    browserSync.init({
+        server: {
+          baseDir: "dist/",
+          directory: false
+        },
+        serveStatic: ['./dist'],
+        open: false
+    });
+})
+
+gulp.task('reload', function(done){
+    browserSync.reload();
+    done();
+});
+
 gulp.task('build', function(){
-   gulp.run('scripts','styles','html');
+   gulp.run('scripts','styles','html', 'assets');
 });
 
 gulp.task('default', function() {
-    gulp.run('lr-server', 'scripts', 'styles', 'html');
+    gulp.run('lr-server', 'scripts', 'styles', 'html', 'assets', 'reload');
 
     gulp.watch('app/src/**', function(event) {
-        gulp.run('scripts');
+        gulp.run('scripts', 'reload');
     })
 
     gulp.watch('app/css/**', function(event) {
-        gulp.run('styles');
+        gulp.run('styles', 'reload');
     })
 
     gulp.watch('app/**/*.html', function(event) {
-        gulp.run('html');
+        gulp.run('html', 'reload');
+    })
+
+    gulp.watch('app/assets/**/*', function(event) {
+        gulp.run('assets', 'reload');
     })
 })
